357bceaa150f3756b1dfeaefcfacaca27cdf03b1
lermanori <lermanori@gmail.com>
2018-01-25 18:05:19 +0200

    Update unitest.cpp

--- unitest.cpp
+++ unitest.cpp
@@ -1,43 +1,123 @@
+
 #include "Mytrie.h"
- #include <iostream>
 
-void main() 
+
+typedef std::pair<KeyType, int > pair;
+//ori lerman is a bitch.
+int main()
 {
+	std::vector<pair> vec;
+	pair temp;
+	temp.second = 1;
+	bool exists;
+	bool word = false;
+	char ch = '/';
+
+	//ch = getchar();
+
+	ch = getchar();
+	while (ch != '.')
+	{
+		word = false;
+		temp.first.clear();
+		exists = false;
+
+		if (ch == ' ' || ch=='\n')
+		{
+			ch = getchar();
+		}
+		else if (ch == '.')
+			break;
+		else
+		{
+			while (ch != ' ' && ch != '.'&&ch!='\n')
+			{
+
+				temp.first.push_back(ch);
+				ch = getchar();
+
+			}
+			word = true;
+
+		}
+		if (word)
+		{
+
+			for (pair& t : vec)
+				if (t.first.compare(temp.first) == 0)
+				{
+					t.second++;
+					exists = true;
+				}
+			if (!exists)
+			{
+				vec.push_back(temp);
+			}
+
+		}
+
+
+	}
+
+
 	Mytrie m;
-	m.insert("ori", 1);
-	m.insert("ob", 1);
-	m.insert("oriX", 1);
-	m.insert("orly", 1);
-	m.insert("alp", 2);
-	m.insert("ox", 1);
-	int i = m.find("ori");
-	std::cout <<"ori:"<< i<<std::endl;
-	 i = m.find("ob");
-	std::cout << "ob:" << i << std::endl;
-	 i = m.find("orix");
-	std::cout << "orix:" << i << std::endl;
-	 i = m.find("alp");
-	std::cout << "alp:" << i << std::endl;
-	 i = m.find("oril");
-	std::cout << "oril:" << i << std::endl;
-	
-	m.Delete("ori");
-	i = m.find("ori");
+
+	while (vec.size() != 0)
+	{
+		temp = vec[vec.size() - 1];
+		vec.pop_back();
+		m.insert(temp.first, temp.second);
+	}
+
+	ch = '/';//initialization between stages
+	temp.first.clear();
+	temp.second = 0;
+	std::cin.clear();
+	std::string dummy;
+	std::getline(std::cin, dummy, '\n');
+	ch = getchar();
+
+	while (ch != '.')//second stage
+	{
+		word = false;
+		temp.first.clear();
+
+		if (ch == ' ' || ch == '\n')
+		{
+			ch = getchar();
+		}
+		else if (ch == '.')
+			break;
+		else
+		{
+			while (ch != ' ' && ch != '.'&&ch != '\n')
+			{
+
+				temp.first.push_back(ch);
+				ch = getchar();
+
+			}
+			word = true;
+
+		}
+		if (word)
+		{
+			int apperences = m.find(temp.first);
+			if (apperences != 0)
+			{
+				std::cout << apperences << std::endl;
+				m.Delete(temp.first);
+			}
+			else
+				std::cout << "did you mean " << m.approxFind(temp.first)<<"?" << std::endl;
+			
+		}
+	}
+
 	
 
-	i = m.find("orix");
-	std::cout << "orix bf deltet:"<< i << std::endl;
-	m.Delete("oriX");
-	std::cout << "orix after deltet:" << i << std::endl;
-	
-	i = m.find("alp");
-	std::cout << "alp bf deltet:" << i << std::endl;
-	m.Delete("ALP");
-	std::cout << "alp after deltet:" << i << std::endl;
 
-	i = m.find("alp");
-	std::cout << "alp bf 2 deltet:" << i << std::endl;
-	m.Delete("ALP");
-	std::cout << "alp after 2 deltet:" << i << std::endl;
+	return 1;
 
 };
+
